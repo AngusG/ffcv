892bc26bd4015883f204f7098bc78eded56b5684
gallowaa <gallowaa@mail.uoguelph.ca>
2016-01-04 17:32:31 -0500

    fastcvdemo working in android studio

---
+++ fastcvdemo1/app/src/main/obj/local/armeabi/objs-debug/fastcvUtils/FPSCounter.o.d
@@ -0,0 +1,65 @@
+obj/local/armeabi/objs-debug/fastcvUtils/FPSCounter.o: \
+ jni/utils/FPSCounter.cpp \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/time.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/utils/FPSCounter.h
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/time.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+C:/Development/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/utils/FPSCounter.h:
